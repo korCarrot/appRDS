20c58dbd53b795e8ffde9350926c8e9cf56f6b6b
korCarrot <rlawjdrms30@gmail.com>
2024-06-03 10:21:55 +0900

    2024_06_03

--- src/test/java/com/boot/apprds/AppRdsApplicationTests.java
+++ src/test/java/com/boot/apprds/AppRdsApplicationTests.java
@@ -1,13 +1,36 @@
 package com.boot.apprds;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @SpringBootTest
 class AppRdsApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     void contextLoads() {
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+            ResultSet resultSet = preparedStatement.executeQuery();){
+
+            resultSet.next();
+
+            System.out.println(resultSet.getString(1));
+
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
